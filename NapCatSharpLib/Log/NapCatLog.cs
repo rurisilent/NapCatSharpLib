3d0710547a43e7a8b2fc238158f52ddfde81d21f
rurisilent <rurisilent@gmail.com>
2026-02-28 00:49:09 +0800

    fix(git): 加入 Logger

---
+++ NapCatSharpLib/Log/NapCatLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NapCatSharpLib.Log
+{
+    public enum NapCatLogType
+    {
+        normal,
+        warning,
+        error
+    }
+
+    public delegate void EvtNapCatLog(string content, NapCatLogType type);
+}
